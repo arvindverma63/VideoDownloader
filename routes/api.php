a3692b8fa3c6e6e363faf0e03277d15a494e4e48
ARVIND_VERMA <arvindverma630635@gmail.com>
2025-02-16 14:30:40 +0530

    new method for auth added

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,9 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-Route::post('/verify-otp', [AuthController::class, 'verifyPasswordOtp']);
+Route::post('/verify-password-otp', [AuthController::class, 'verifyPasswordOtp']);
 Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+Route::post('/verify-otp',[AuthController::class,'verfiyOtp']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
